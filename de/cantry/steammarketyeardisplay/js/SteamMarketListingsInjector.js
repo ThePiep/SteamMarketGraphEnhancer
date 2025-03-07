e48e067ea2419dc408a4e8b1deabfce1b9b07d4f
Kaj van Rijn <k.d.vanrijn@student.utwente.nl>
2025-03-07 15:51:07 +0100

    Update manifesto to version 3

--- de/cantry/steammarketyeardisplay/js/SteamMarketListingsInjector.js
+++ de/cantry/steammarketyeardisplay/js/SteamMarketListingsInjector.js
@@ -1,22 +1,28 @@
 var observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        if (!mutation.addedNodes) return
-        for (let i = 0; i < mutation.addedNodes.length; i++) {
-            let node = mutation.addedNodes[i];
-            if (node.tagName === "SCRIPT") {
-                if (node.getAttribute("src") != null && node.getAttribute("src").includes("economy.js")) {
-                    let element = document.createElement("script");
-                    element.setAttribute("src", chrome.extension.getURL("js/OverwrittenFunction.js"));
-                    document.head.appendChild(element);
-                }
-            }
+  mutations.forEach(function (mutation) {
+    if (!mutation.addedNodes) return;
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      let node = mutation.addedNodes[i];
+      if (node.tagName === "SCRIPT") {
+        if (
+          node.getAttribute("src") != null &&
+          node.getAttribute("src").includes("economy.js")
+        ) {
+          let element = document.createElement("script");
+          element.setAttribute(
+            "src",
+            chrome.runtime.getURL("js/OverwrittenFunction.js")
+          );
+          document.head.appendChild(element);
         }
-    })
-})
+      }
+    }
+  });
+});
 
 observer.observe(document, {
-    childList: true
-    , subtree: true
-    , attributes: false
-    , characterData: false
-})
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false,
+});
